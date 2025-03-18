fec254c9d1999e04ddcf23b5883cb000abe50ef9
saintbus <trinbus9291@gmail.com>
2025-03-18 15:34:45 +0700

    test2

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -3,13 +3,11 @@ import Link from "next/link";
 
 export default async function VenueCatalog({venueJson}:{venueJson:Promise<VenueJson>}) {
 
-    const venueJsonResolve = await venueJson
-
     return(
         <div style={{margin:"20px", display:"flex", flexDirection:"row", 
             flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
             {
-                venueJsonResolve.data.map((venueItem) =>(
+                (await venueJson).data.map((venueItem) =>(
                     <Link href={`/venue/${venueItem._id}`} className="w-1/5" key={venueItem._id}>
                     <Card venueName={venueItem.name} imgSrc={venueItem.picture} key={venueItem._id}/>
                     </Link>
